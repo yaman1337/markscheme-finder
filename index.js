be43ea62fd23f586a52c8ccfc0e70e1f6d553058
yaman1337 <yaman.sarabariya@gmail.com>
2022-09-09 23:37:41 +0545

    year < 10 errors fixed

--- index.js
+++ index.js
@@ -1,38 +1,56 @@
 import fetch from "node-fetch";
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 
 const app = express();
-app.use(cors())
-app.use(express.static("public"))
+app.use(cors());
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.sendFile("/index.html")
-})
+  res.sendFile("/index.html");
+});
 
 app.get("/api", async (req, res) => {
-    try {
+  try {
+    let { season, year, variant } = req.query;
+    season = season.toLowerCase();
+    year = parseInt(year);
+    let url = `https://dynamicpapers.com/wp-content/uploads/2015/09/9701_${season}${
+      year < 10 ? 0 : ""
+    }${year}_ms_${variant}.pdf`;
 
-        let {season, year, variant} = req.query;
-        season = season.toLowerCase()
+    console.log({ season, year });
 
-        const resp = await fetch(
-            `https://dynamicpapers.com/wp-content/uploads/2015/09/9701_${season}${year}_ms_${variant}.pdf`
-          );
-          
-          if(resp.status > 300) {
-           return res.status(404).json({error: "Marks scheme not found"})
-          }
+    let paper = parseInt(variant.split("")[0]);
 
-          const data = await resp.arrayBuffer()
-          const final = "data:application/pdf;base64, "+Buffer.from(data).toString('base64');
-          res.send(final)
-    } catch (error) {
-        console.log(error);
-        res.send(error)
+    if (year === 4 && season === "s") {
+      url = `https://dynamicpapers.com/wp-content/uploads/2015/09/9701_${season}0${year}_ms.pdf`;
+    } else if (year === 2 || (year <= 9 && year >= 4)) {
+      url = `https://dynamicpapers.com/wp-content/uploads/2015/09/9701_${season}0${year}_ms_${paper}.pdf`;
+    } else if (year === 4 && season === "s") {
+      url = `https://dynamicpapers.com/wp-content/uploads/2015/09/9701_${season}0${year}_ms.pdf`;
+    } else if (year < 4) {
+      url = `https://dynamicpapers.com/wp-content/uploads/2015/09/9701_${season}0${year}_ms_1-2-3-4-5-6.pdf`;
     }
-})
+
+    console.log(url);
+
+    const resp = await fetch(url);
+
+    if (resp.status > 300) {
+      return res.status(404).json({ error: "Marks scheme not found" });
+    }
+
+    const data = await resp.arrayBuffer();
+    const final =
+      "data:application/pdf;base64, " + Buffer.from(data).toString("base64");
+    res.send(final);
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+});
 
 app.listen(process.env.PORT || 9000, () => {
-    console.log("Listening on port 9000")
-})
+  console.log("Listening on port 9000");
+});
