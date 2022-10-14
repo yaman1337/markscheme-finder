807ce618183f5e05368e1d1a3c473a3068131cc2
abinash2525 <88371116+abinash2525@users.noreply.github.com>
2022-10-14 14:39:05 +0545

    allow custom domains in cors

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-app.use(cors());
+app.use(cors({origin: ["*"]}));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
